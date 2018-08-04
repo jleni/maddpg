dc0377cf18b887e500979cf112d17a11d1f9719d
Juan Leni <lenijuan@gmail.com>
2018-08-04 20:37:13 +0200

    cleaning up maddpg implementation

--- maddpg/trainer/maddpg.py
+++ maddpg/trainer/maddpg.py
@@ -27,123 +27,6 @@ def make_update_exp(vals, target_vals):
     return U.function([], [], updates=[expression])
 
 
-def p_train(obs_ph_n, act_space_n, agent_index, p_func, q_func, p_optimizer, grad_norm_clipping=None,
-            local_q_func=False, num_units=64):
-    ##################
-    # create distributions
-    act_pdtype_n = [make_pdtype(act_space) for act_space in act_space_n]
-
-    ##################
-    # set up placeholders
-    act_ph_n = [act_pdtype_n[i].sample_placeholder([None], name="action" + str(i)) for i in range(len(act_space_n))]
-
-    ##################
-    # p_func
-    p_input = obs_ph_n[agent_index]
-
-    ##################
-    # p_func
-    p = p_func(p_input, int(act_pdtype_n[agent_index].param_shape()[0]), scope="p_func", num_units=num_units)
-    p_func_vars = U.scope_vars(U.absolute_scope_name("p_func"))
-
-    # wrap parameters in distribution
-    act_pd = act_pdtype_n[agent_index].pdfromflat(p)
-
-    with tf.variable_scope("act_sample", reuse=None):
-        act_sample = act_pd.sample()
-
-    with tf.variable_scope("p_reg", reuse=None):
-        p_reg = tf.reduce_mean(tf.square(act_pd.flatparam()))
-
-    act_input_n = act_ph_n + []
-    act_input_n[agent_index] = act_pd.sample()
-
-    q_input = tf.concat(obs_ph_n + act_input_n, 1)
-    if local_q_func:
-        q_input = tf.concat([obs_ph_n[agent_index], act_input_n[agent_index]], 1)
-    q = q_func(q_input, 1, scope="q_func", reuse=True, num_units=num_units)[:, 0]
-
-    pg_loss = -tf.reduce_mean(q)
-
-    loss = pg_loss + p_reg * 1e-3
-
-    optimize_expr = U.minimize_and_clip(p_optimizer, loss, p_func_vars, grad_norm_clipping)
-
-    # Create callable functions
-    p_train_func = U.function(inputs=obs_ph_n + act_ph_n, outputs=loss, updates=[optimize_expr])
-    act = U.function(inputs=[obs_ph_n[agent_index]], outputs=act_sample)
-    p_values = U.function([obs_ph_n[agent_index]], p)
-
-    # target network
-    target_p = p_func(p_input, int(act_pdtype_n[agent_index].param_shape()[0]), scope="target_p_func",
-                      num_units=num_units)
-    target_p_func_vars = U.scope_vars(U.absolute_scope_name("target_p_func"))
-    update_target_p = make_update_exp(p_func_vars, target_p_func_vars)
-
-    target_act_sample = act_pdtype_n[agent_index].pdfromflat(target_p).sample()
-    target_act = U.function(inputs=[obs_ph_n[agent_index]], outputs=target_act_sample)
-
-    p_debug = {'p_values': p_values, 'target_act': target_act}
-    return act, p_train_func, update_target_p, p_debug
-
-
-def q_train(obs_ph_n, act_space_n, agent_index, q_func, q_optimizer, q_grad_norm_clipping=None, local_q_func=False,
-            num_units=64):
-    ##################
-    # create distributions
-    act_pdtype_n = [make_pdtype(act_space) for act_space in act_space_n]
-
-    ##################
-    # set up placeholders
-    act_ph_n = [act_pdtype_n[i].sample_placeholder([None], name="action" + str(i)) for i in range(len(act_space_n))]
-    target_ph = tf.placeholder(tf.float32, [None], name="target")
-
-    ##################
-    # input
-    q_input = tf.concat(obs_ph_n + act_ph_n, 1)
-
-    ##################
-    # q_func
-    if local_q_func:
-        q_input = tf.concat([obs_ph_n[agent_index], act_ph_n[agent_index]], 1)
-    tmp = q_func(q_input, 1, scope="q_func", num_units=num_units)
-
-    with tf.variable_scope("q", reuse=None):
-        q = tmp[:, 0]
-
-    q_func_vars = U.scope_vars(U.absolute_scope_name("q_func"))
-
-    with tf.variable_scope("q_loss", reuse=None):
-        q_loss = tf.reduce_mean(tf.square(q - target_ph))
-
-    # viscosity solution to Bellman differential equation in place of an initial condition
-    with tf.variable_scope("q_reg", reuse=None):
-        q_reg = tf.reduce_mean(tf.square(q))
-
-    loss = q_loss  # + 1e-3 * q_reg
-
-    optimize_expr = U.minimize_and_clip(q_optimizer, loss, q_func_vars, q_grad_norm_clipping)
-
-    # Create callable functions
-    q_train_func = U.function(inputs=obs_ph_n + act_ph_n + [target_ph], outputs=loss, updates=[optimize_expr])
-    q_values = U.function(obs_ph_n + act_ph_n, q)
-
-    # target network
-    tmp = q_func(q_input, 1, scope="target_q_func", num_units=num_units)
-    with tf.variable_scope("target_q", reuse=None):
-        target_q = tmp[:, 0]
-    target_q_func_vars = U.scope_vars(U.absolute_scope_name("target_q_func"))
-
-    with tf.variable_scope("q_update", reuse=None):
-        update_target_q = make_update_exp(q_func_vars, target_q_func_vars)
-
-    target_q_values = U.function(obs_ph_n + act_ph_n, target_q)
-
-    q_debug = {'q_values': q_values, 'target_q_values': target_q_values}
-
-    return q_train_func, update_target_q, q_debug
-
-
 class MADDPGAgentTrainer(AgentTrainer):
     def __init__(self, name, model, obs_shape_n, act_space_n, agent_index, args, local_q_func=False):
         self.name = name
@@ -153,7 +36,7 @@ class MADDPGAgentTrainer(AgentTrainer):
 
         obs_ph_n = []
         for i in range(self.n):
-            obs_ph_n.append(U.BatchInput(obs_shape_n[i], name="obs" + str(i)).get())
+            obs_ph_n.append(U.BatchInput(obs_shape_n[i], name="obs{}".format(i)).get())
 
         q_func = model
         q_optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
@@ -163,32 +46,111 @@ class MADDPGAgentTrainer(AgentTrainer):
         p_optimizer = tf.train.AdamOptimizer(learning_rate=args.lr)
         p_grad_norm_clipping = 0.5
 
+        act = None
+        p_train_func = None
+        update_target_p = None
+        p_debug = None
+
         num_units = args.num_units
 
         with tf.variable_scope(self.name, reuse=None):
-            # Create all the functions necessary to train the model
-            q_train_func, update_target_q, q_debug = q_train(
-                obs_ph_n=obs_ph_n,
-                act_space_n=act_space_n,
-                agent_index=agent_index,
-                q_func=q_func,
-                q_optimizer=q_optimizer,
-                q_grad_norm_clipping=q_grad_norm_clipping,
-                local_q_func=local_q_func,
-                num_units=num_units
-            )
+            ##################
+            # create distributions
+            act_pdtype_n = [make_pdtype(act_space) for act_space in act_space_n]
+            act_space_len = len(act_space_n)
 
-            act, p_train_func, update_target_p, p_debug = p_train(
-                obs_ph_n=obs_ph_n,
-                act_space_n=act_space_n,
-                agent_index=agent_index,
-                p_func=p_func,
-                q_func=q_func,
-                p_optimizer=p_optimizer,
-                grad_norm_clipping=p_grad_norm_clipping,
-                local_q_func=local_q_func,
-                num_units=num_units
-            )
+            ##################
+            # set up placeholders
+            act_ph_n = [act_pdtype_n[i].sample_placeholder([None], name="action{}".format(i)) for i in range(act_space_len)]
+            target_ph = tf.placeholder(tf.float32, [None], name="target")
+
+            ##################
+            # input
+            with tf.variable_scope("obs_and_act", reuse=None):
+                q_input = tf.concat(obs_ph_n + act_ph_n, 1)
+            p_input = obs_ph_n[agent_index]
+            if local_q_func:
+                q_input = tf.concat([obs_ph_n[agent_index], act_ph_n[agent_index]], 1)
+
+            ##################
+            # q_func / p_func
+            tmp = q_func(q_input, 1, scope="q_func", num_units=num_units)
+            with tf.variable_scope("q", reuse=None):
+                q = tmp[:, 0]
+
+            p = p_func(p_input, int(act_pdtype_n[agent_index].param_shape()[0]), scope="p_func", num_units=num_units)
+
+            q_func_vars = U.scope_vars(U.absolute_scope_name("q_func"))
+            p_func_vars = U.scope_vars(U.absolute_scope_name("p_func"))
+
+            with tf.variable_scope("q_loss", reuse=None):
+                q_loss = tf.reduce_mean(tf.square(q - target_ph))
+
+                # viscosity solution to Bellman differential equation in place of an initial condition
+                q_reg = tf.reduce_mean(tf.square(q))
+
+            loss = q_loss  # + 1e-3 * q_reg
+
+            q_optimize_expr = U.minimize_and_clip(q_optimizer, loss, q_func_vars, q_grad_norm_clipping)
+
+            # Create callable functions
+            q_train_func = U.function(inputs=obs_ph_n + act_ph_n + [target_ph], outputs=loss, updates=[q_optimize_expr])
+            q_values = U.function(obs_ph_n + act_ph_n, q)
+
+            # target network
+            tmp = q_func(q_input, 1, scope="target_q_func", num_units=num_units)
+            with tf.variable_scope("target_q", reuse=None):
+                target_q = tmp[:, 0]
+            target_q_func_vars = U.scope_vars(U.absolute_scope_name("target_q_func"))
+
+            with tf.variable_scope("q_update", reuse=None):
+                update_target_q = make_update_exp(q_func_vars, target_q_func_vars)
+
+            target_q_values = U.function(obs_ph_n + act_ph_n, target_q)
+
+            q_debug = {'q_values': q_values, 'target_q_values': target_q_values}
+
+            ###############################################################################################
+            ###############################################################################################
+            ###############################################################################################
+
+            # wrap parameters in distribution
+            act_pd = act_pdtype_n[agent_index].pdfromflat(p)
+
+            with tf.variable_scope("act_sample", reuse=None):
+                act_sample = act_pd.sample()
+
+            with tf.variable_scope("act_n_sample", reuse=None):
+                act_input_n = act_ph_n + []
+                act_input_n[agent_index] = act_pd.sample()
+
+            with tf.variable_scope("p_loss", reuse=None):
+                pg_loss = -tf.reduce_mean(q)
+                with tf.variable_scope("p_reg", reuse=None):
+                    p_reg = tf.reduce_mean(tf.square(act_pd.flatparam()))
+                p_loss = pg_loss + p_reg * 1e-3
+
+            p_optimize_expr = U.minimize_and_clip(p_optimizer, p_loss, p_func_vars, p_grad_norm_clipping)
+
+            # Create callable functions
+            p_train_func = U.function(inputs=obs_ph_n + act_ph_n, outputs=p_loss, updates=[p_optimize_expr])
+            act = U.function(inputs=[obs_ph_n[agent_index]], outputs=act_sample)
+            p_values = U.function([obs_ph_n[agent_index]], p)
+
+            # target network
+            target_p = p_func(p_input, int(act_pdtype_n[agent_index].param_shape()[0]),
+                              scope="target_p_func",
+                              num_units=num_units)
+            target_p_func_vars = U.scope_vars(U.absolute_scope_name("target_p_func"))
+
+            with tf.variable_scope("p_update", reuse=None):
+                update_target_p = make_update_exp(p_func_vars, target_p_func_vars)
+
+            with tf.variable_scope("target_act_sample", reuse=None):
+                target_act_sample = act_pdtype_n[agent_index].pdfromflat(target_p).sample()
+            target_act = U.function(inputs=[obs_ph_n[agent_index]], outputs=target_act_sample)
+
+            p_debug = {'p_values': p_values, 'target_act': target_act}
 
         self.q_train = q_train_func
         self.q_update = update_target_q
@@ -241,7 +203,11 @@ class MADDPGAgentTrainer(AgentTrainer):
             target_q_next = self.q_debug['target_q_values'](*(obs_next_n + target_act_next_n))
             target_q += rew + self.args.gamma * (1.0 - done) * target_q_next
         target_q /= num_sample
-        q_loss = self.q_train(*(obs_n + act_n + [target_q]))
+
+        try:
+            q_loss = self.q_train(*(obs_n + act_n + [target_q]))
+        except Exception as e:
+            pass
 
         # train p network
         p_loss = self.p_train(*(obs_n + act_n))
